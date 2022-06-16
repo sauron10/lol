db60f817cf6ece19043d7c1e326808886c4c8473
heivagar <heivagar@gmail.com>
2022-06-15 19:27:41 -0500

    Change api route

--- front/client/src/page-assets/route.js
+++ front/client/src/page-assets/route.js
@@ -2,5 +2,5 @@ export const vars = (() => {
   const dev = process.env.NODE_ENV === 'development'
   const vars =  {axiosConf:{validateStatus:status => status<500}}
   if(dev) return {...vars,route: 'http://localhost:8080',}
-  return {...vars,route: ''} 
+  return {...vars,route: '/api'} 
 })()
